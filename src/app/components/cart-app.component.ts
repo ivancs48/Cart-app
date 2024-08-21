034ef86c59992b5dffdfac402314cb0dbad49aa5
Ivan Calderon <iasuarez@worldoffice.com.co>
2024-08-21 14:33:33 -0500

    Se crea pagina inicial de catalogo con boostrap

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'cart-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-app.component.html',
+})
+export class CartAppComponent implements OnInit {
+  
+  products: Product[] = [];
+
+  constructor(private service: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
+
+}
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,39 @@
+import { Product } from '../models/product';
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'Teclado mecanico asus RGB',
+    description: 'Teclado mecanico con luces RGB cherry red',
+    price: 30.99,
+  },
+  { id: 2, 
+    name: 'Samsung Samrt TV LED 75',
+    description: 'Algun LCD excelente OLED Screen',
+    price: 150.99 
+
+  },
+  { id: 3, 
+    name: 'Nvidia Asus TRX4900',
+    description: 'Tarheta de video Nvidia optimizada',
+    price: 70.99 
+
+  },
+  { id: 4, 
+    name: 'TSony Camara Video',
+    description: 'Camara de extreamo video',
+    price: 75.99 
+
+  },
+  { id: 5, 
+    name: 'Corsair memoria ram 16 GB DDr5',
+    description: 'Memoria ram para video juegos',
+    price: 50.99 
+
+  },
+  { id: 6, 
+    name: 'CPU Intel core i7',
+    description: 'CPU optimizado multi tarea',
+    price: 80.99 
+  }
+];
